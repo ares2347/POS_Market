af0060e0ac946c4beb6034bd3fa502136677188c
TongMinhDuong <101384330+TongMinhDuong@users.noreply.github.com>
2022-10-25 21:03:25 +0700

    scene add

---
+++ src/billsHistory/BillsHistoryController.java
@@ -0,0 +1,20 @@
+package billsHistory;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class BillsHistoryController {
+    public TextField txtSearch;
+    public TableColumn tdNameCustomer;
+    public TableColumn tdDate;
+    public TableColumn tdId;
+    public TableView tbvBill;
+
+    public void goToNewBill(ActionEvent actionEvent) {
+    }
+
+    public void handleSearch(ActionEvent actionEvent) {
+    }
+}

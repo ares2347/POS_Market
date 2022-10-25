160ff24d36f5571e933cbc9a60a0db581366507f
trant <trantu2000hp@gmail.com>
2022-10-25 10:55:39 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import helper.DefaultScene;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static Stage rootStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        rootStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        primaryStage.setTitle("POS Market");
+        primaryStage.setScene(new DefaultScene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}

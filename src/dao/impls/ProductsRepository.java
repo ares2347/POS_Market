302fc10fe52497ede1278ad5b035d8c353938f1e
towly249 <caonamtq249@gmail.com>
2022-10-31 21:08:48 +0700

    31/10

--- src/dao/impls/ProductsRepository.java
+++ src/dao/impls/ProductsRepository.java
@@ -52,15 +52,56 @@ public class ProductsRepository implements IRepository<Products> {
 
     @Override
     public boolean update(Products products) {
+        try {
+            String sql_txt = "update products set name=?, quantity=?,price=? where id=?";
+            Connector conn = Connector.getInstance();
+            ArrayList arr = new ArrayList();
+            arr.add(products.getName());
+            arr.add(products.getQuantity());
+            arr.add(products.getPrice());
+            arr.add(products.getId());
+            if(conn.execute(sql_txt,arr)){
+                return true;
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
         return false;
     }
 
     @Override
     public boolean delete(Products products) {
+        try {
+            String sql_txt = "delete from products where id=?";
+            Connector conn = Connector.getInstance();
+            ArrayList arr = new ArrayList();
+            arr.add(products.getId());
+            if(conn.execute(sql_txt,arr)){
+                return true;
+            }
+        }catch (Exception e){
+        }
         return false;
     }
 
     @Override
-    public Products findOne(Integer id){return null;}
+    public Products findOne(Integer id){
+        try {
+            String sql_txt = "select * from products where id=?";
+            Connector conn = Connector.getInstance();
+            ArrayList arr = new ArrayList();
+            arr.add(id);
+            ResultSet rs = conn.executeQuery(sql_txt,arr);
+            while (rs.next()) {
+                int Id = rs.getInt("id");
+                String name = rs.getString("name");
+                int price  = rs.getInt("price");
+                int quantity = rs.getInt("quantity");
+                return new Products(Id, name,quantity , price);
+            }
+        }catch (Exception e){
+        }
+        return null;
+    }
 
 }

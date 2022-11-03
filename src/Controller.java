52487e1d1f450e38391a13e37f942533b7c8186e
xuanhanhbn <101380759+xuanhanhbn@users.noreply.github.com>
2022-11-03 15:41:46 +0700

    update finalCost

--- src/Controller.java
+++ src/Controller.java
@@ -3,14 +3,14 @@ import entities.Products;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.text.Text;
+import javafx.util.converter.IntegerStringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -38,18 +38,34 @@ public class Controller implements Initializable {
     private TextField txtQuantity;
     @FXML
     private TextField txtPrice;
+    public Button test;
+    ObservableList<Products> pr = FXCollections.observableArrayList();
 
+    public float finalCost(){
+        float total = 0;
+        for (Products p: pr) {
+            float entryCost = p.getQuantity() * p.getPrice();
+            total = total + entryCost;
+        }
+        return total;
+    }
     @FXML
     private void handleAddProduct(ActionEvent actionEvent) {
+        float total = 0;
         Float price = Float.parseFloat(txtPrice.getText());
         Integer qty = Integer.parseInt(txtQuantity.getText());
         String s = cboSelectNameProduct.getValue().getName();
         Products newProducts = new Products(null, s, price, qty);
 
+        pr.add(newProducts);
         tbvAddProduct.getItems().add(newProducts);
+//        total = qty * price;
+//        finalCost = finalCost + total;
+        txTotal.setText(String.valueOf(finalCost()));
 
-        System.out.println("qty: "+qty);
+
     }
+
 
 
     @FXML
@@ -73,13 +89,37 @@ public class Controller implements Initializable {
         tdPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
         tdSubTotal.setCellValueFactory(new PropertyValueFactory<>("subtotal"));
 
+//        test.setOnAction(e -> {
+//            this.txTotal.setText(String.valueOf(finalCost()));
+//        });
+//        tbvAddProduct.setItems(pr);
+
+//        for (Products p : pr) {
+//            finalCost = finalCost + p.getSubtotal();
+//            System.out.println("FinalCost: "+finalCost);
+//        }
+//                this.txTotal.setText(String.valueOf(finalCost));
 
         Repository rp = new Repository();
         ObservableList<Products> pr = FXCollections.observableArrayList();
         pr.addAll(rp.all());
         cboSelectNameProduct.setItems(pr);
+        tbvAddProduct.setEditable(true);
+
+        tdQuantity.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        tdQuantity.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Products, Integer>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Products, Integer> event) {
+                float total = 0;
+                Products pr = event.getRowValue();
+                pr.setQuantity(event.getNewValue());
+
+                tbvAddProduct.refresh();
+                txTotal.setText(String.valueOf(finalCost()));
 
 
+            }
+        });
     }
 
     public void choose(ActionEvent actionEvent) {
